1a1ae2756e81be16d84e2859436a9a72a671b076
BartoszKut <bartosz.kut.programista@gmail.com>
2021-08-23 16:27:12 +0200

    Refactoring JS code 2

--- App/Config.php
+++ App/Config.php
@@ -32,7 +32,7 @@ class Config
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'Mikasa02Mikasa02';
+    const DB_PASSWORD = '';
 
     /**
      * Show or hide error messages on screen
